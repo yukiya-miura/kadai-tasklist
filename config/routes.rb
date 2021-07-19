0413815f732db47c2307849ca67621d3d6ef9574
yukiya.miura <2511kagurazaka@gmail.com>
2021-07-19 01:43:50 +0000

    top page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+  get 'toppages/index'
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-    root to: 'tasks#index'
+    root to: 'toppages#index'
+    
+    get 'login', to: 'sessions#new'
+    post 'login', to: 'sessions#create'
+    delete 'logout', to: 'sessions#destroy'
     
     resources :tasks  
 end
