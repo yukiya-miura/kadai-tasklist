e890320fd06160e0e7525df0421d05ff3a46a5fd
yukiya.miura <2511kagurazaka@gmail.com>
2021-07-21 07:26:32 +0000

    user edit deleted

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
-  resources :users, only: [:edit, :update, :create, :destroy]
+  resources :users, only: [:create]
   resources :tasks  
 end
