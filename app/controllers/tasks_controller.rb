189f4ad89ad12e0e49d3a1fd40df2382192a1134
yukiya.miura <2511kagurazaka@gmail.com>
2021-07-21 06:56:25 +0000

    before_action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,4 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
   before_action :require_user_logged_in
   before_action :correct_user, only: [:show, :edit, :update, :destroy]
   
@@ -52,15 +51,14 @@ class TasksController < ApplicationController
   
   private
   
-  def set_task
-    @task = Task.find(params[:id])
-  end
+
   
   def task_params
       params.require(:task).permit(:content, :status)
   end
   
   def correct_user
+    @task = Task.find(params[:id])
     if @task.user_id == current_user.id
     else
     redirect_to root_url
